8bace7d0028694600fa8eedfeabecb9524a64af4
Dev <park@seung.dev>
2021-01-14 15:18:36 -0800

    Project startup

---
+++ src/pages/CreditCards.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreditCards = () => {
+
+    return (
+        <div className="credit-cards">
+            
+        </div>
+    );
+}
+
+export default CreditCards;
